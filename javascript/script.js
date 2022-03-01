5b7e87380dddce4271e4c4ed199e55041997d4ec
AdeAde99 <adelintanasa.d@gmail.com>
2022-03-01 17:19:31 +0100

    esercizio Pali senza funzioni errore undefined

--- javascript/script.js
+++ javascript/script.js
@@ -19,3 +19,18 @@ for(i = wordLength; i > 0; i--) {
     ArrWordReadFromRight.push(word[i]);
 }
 console.log(ArrWordReadFromRight);
+
+
+let WordReadFromLeft = ArrWordReadFromLeft.toString();
+console.log(WordReadFromLeft);
+let WordReadFromRight = ArrWordReadFromRight.toString();
+console.log(WordReadFromRight);
+
+
+
+
+if (WordReadFromLeft == WordReadFromRight) {
+    console.log('La parola è palindroma');
+} else {
+    console.log('La parola non è palindroma');
+}
